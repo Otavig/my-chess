e08a490069f45f5858713d72b22902c8c383fb8c
otavig <otaviogarcia.santos4@gmail.com>
2025-06-08 17:04:33 -0300

    feet: Pawn valid moves

--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -1,5 +1,6 @@
 package piece;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import utils.Board;
@@ -20,6 +21,35 @@ public class Pawn extends Piece {
 
 	@Override
 	public List<Position> getMovesValid(Board board, int row, int col) {
-		return null;
+		List<Position> moves = new ArrayList<>();
+	    int direction = color.equals("white") ? 1 : -1;
+
+	    int nextRow = row + direction;
+
+	    // Movimento normal de 1 casa
+	    if (board.isInsideTheBoard(nextRow, col) &&
+	        board.getPiece(nextRow, col) == null) {
+	        moves.add(new Position(nextRow, col));
+
+	        // Movimento de 2 casas (apenas se ainda não moveu)
+	        int twoStepsRow = row + 2 * direction;
+	        if (!moved && board.isInsideTheBoard(twoStepsRow, col) &&
+	            board.getPiece(twoStepsRow, col) == null) {
+	            moves.add(new Position(twoStepsRow, col));
+	        }
+	    }
+
+	    // Capturas diagonais
+	    for (int dCol = -1; dCol <= 1; dCol += 2) {
+	        int diagCol = col + dCol;
+	        if (board.isInsideTheBoard(nextRow, diagCol)) {
+	            Piece target = board.getPiece(nextRow, diagCol);
+	            if (target != null && !target.getColor().equals(this.color)) {
+	                moves.add(new Position(nextRow, diagCol));
+	            }
+	        }
+	    }
+	    
+	    return moves;
 	}
 }
